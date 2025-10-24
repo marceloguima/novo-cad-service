3a98d8fbc720ada39156bd472ccd1f6ad4c9fc2f
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2025-10-23 22:43:44 -0400

    tira erro do arquivo inicial.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Inicial from "./routes/inicial.jsx";
+import Inicial from "./routes/Inicial.jsx";
 import Cadastro from "./routes/Cadastro.jsx";
 import ServicosDoDia from "./routes/ServicosDoDia.jsx";
 import ServicosDaSemana from "./routes/ServicosDaSemana.jsx";
