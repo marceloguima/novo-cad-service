a46971b8fb56283614d3a59e1ffa86456a0c5232
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2025-11-07 08:28:47 -0400

    muda card serviço, agora com botões abaixo dos dados

--- src/components/Card-servico/index.jsx
+++ src/components/Card-servico/index.jsx
@@ -1,25 +1,27 @@
 import "./style.css";
 // import Botao from "../Botao";
 
-
-
 const CardServico = (props) => {
     return (
         <div className="card-servico">
             <div className="content">
                 <div className="dados">
-                    <h2>{props.descricao}</h2>
-                    <p>{props.valor}</p>
-                    <p>{props.data}</p>
+                    <div className="titulo-descricao">
+                        <h2>{props.descricao}</h2>
+                    </div>
+                    <p className="campo-valor"><span>Valor</span>{props.valor}</p>
                     <p>{props.modelo}</p>
                     <p>{props.placa}</p>
+                    <p>{props.data}</p>
                     <p>{props.vendedor}</p>
                 </div>
-                {props.btnMaisDetalhes}
-                {props.btnApagarServico}
- 
             </div>
             {props.obs}
+
+            <div className="butons-card-servico">
+                {props.btnMaisDetalhes}
+                {props.btnApagarServico}
+            </div>
         </div>
     );
 };
